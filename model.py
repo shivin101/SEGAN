5e4b58ab3952c7d1bea8c605c463132fa7a84af0
Shivin <shivin.yadav@gmail.com>
2019-03-08 17:19:37 -0800

    Attention Class

--- model.py
+++ model.py
@@ -3,6 +3,40 @@ import torch.nn as nn
 from torch.nn.modules import Module
 from torch.nn.parameter import Parameter
 
+class Self_Attn(nn.Module):
+    """ Self attention Layer"""
+    def __init__(self,in_dim,activation):
+        super(Self_Attn,self).__init__()
+        self.chanel_in = in_dim
+        self.activation = activation
+        self.channel_div = 8
+        self.query_conv = nn.Conv1d(in_channels = in_dim , out_channels = in_dim//self.channel_div , kernel_size= 1)
+        self.key_conv = nn.Conv1d(in_channels = in_dim , out_channels = in_dim//self.channel_div , kernel_size= 1)
+        self.value_conv = nn.Conv1d(in_channels = in_dim , out_channels = in_dim , kernel_size= 1)
+        self.gamma = nn.Parameter(torch.zeros(1))
+
+        self.softmax  = nn.Softmax(dim=-1) #
+    def forward(self,x):
+        """
+            inputs :
+                x : input feature maps( B X C X W X H)
+            returns :
+                out : self attention value + input feature 
+                attention: B X N X N (N is Width*Height)
+        """
+        m_batchsize,C,width  = x.size()
+        proj_query  = self.query_conv(x).view(m_batchsize,-1,width) # B X CX(N)
+        proj_key =  self.key_conv(x).view(m_batchsize,-1,width) # B X C x (*W*H)
+        energy =  torch.bmm(proj_query,proj_key) # transpose check
+        attention = self.softmax(energy) # BX (N) X (N) 
+        proj_value = self.value_conv(x).view(m_batchsize,-1,width) # B X C X N
+
+        out = torch.bmm(proj_value,attention.permute(0,2,1) )
+        print out.shape()
+        #out = out.view(m_batchsize,C,width,height)
+        
+        out = self.gamma*out + x
+        return out,attention
 
 class VirtualBatchNorm1d(Module):
     """
